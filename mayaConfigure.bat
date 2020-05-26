5a8f1c928f956781d08eb9aa24fabce3d2b33f97
Tyler Fox <tyler@blur.com>
2020-05-26 15:46:37 -0700

    default to maya 2019

--- mayaConfigure.bat
+++ mayaConfigure.bat
@@ -1,7 +1,7 @@
 setlocal
 REM "A quick batch file with variable options to kick off a cmake build on Windows"
 
-SET MAYA_VERSION=2018
+SET MAYA_VERSION=2019
 SET BUILD=mayabuild_%MAYA_VERSION%
 SET COMPILER=Visual Studio 15 2017 Win64
 
@@ -15,6 +15,6 @@ cmake ^
     -DMAYA_VERSION=%MAYA_VERSION% ^
     -G "%COMPILER%" ..\
 
-REM cmake --build . --config Release --target INSTALL
+cmake --build . --config Release --target INSTALL
 
 pause
